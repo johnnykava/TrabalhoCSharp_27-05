3ad7f8f738bd0b7b0312e1bb4b2410783d5b67ff
Johnny Kava <johnnylk2003@hotmail.com>
2025-05-19 14:14:33 -0300

    pasta gerada automatica, que gera o banco de dados a partir de codigo C#

---
+++ Migrations/20250519171050_CriarTabelaFilmes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoDeFilmes.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaFilmes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Duracao = table.Column<int>(type: "INTEGER", nullable: false),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Genero = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Diretor = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
