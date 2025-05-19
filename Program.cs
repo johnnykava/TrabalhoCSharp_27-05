d4f885b53c05551f2b4b3b083e9f6cefcbe1cba8
Johnny Kava <johnnylk2003@hotmail.com>
2025-05-19 14:05:43 -0300

    Configurando a conexão com o banco

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using CatalogoDeFilmes.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Adiciona o serviço que se conecta com o banco de dados o "AppDbContext"
+//O AppDbContext diz:
+//Qual classe vira tabela "Filme"
+//E usar o Sqlite
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=filmes.db"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
